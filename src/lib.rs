d643b5dab51c40872a3523276f297c34d7e66c05
Sam L. Thomas <st@simulacra.to>
2020-05-30 18:40:33 +0200

    support for dockerised IDA

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,12 @@ extern crate failure_derive;
 #[macro_use]
 extern crate serde_derive;
 
-use std::fs::{File, remove_file};
-use std::process;
+use std::fs;
+use std::fs::File;
 use std::io::Write;
-use std::path::Path;
+use std::iter::FromIterator;
+use std::path::{Path, PathBuf};
+use std::process;
 
 use regex::Regex;
 
@@ -20,9 +22,11 @@ use failure::Error;
 #[derive(Debug, Fail)]
 pub enum IdaError {
     #[fail(display = "invalid path to IDA executable: {}", path)]
-    InvalidPath {
-        path: String,
-    },
+    InvalidPath { path: String },
+    #[fail(display = "invalid analysis target: {:?}", path)]
+    InvalidTarget { path: PathBuf },
+    #[fail(display = "invalid analysis target {:?}; would clobber {:?}", path, clobber)]
+    InvalidTargetClobber { path: PathBuf, clobber: PathBuf },
 }
 
 // Exported basic block information
@@ -71,18 +75,40 @@ pub struct IDA {
     bits: Bits,
     mode: Mode,
     wine: bool,
+    docker_image: Option<String>,
+    docker_tag: Option<String>,
+    docker_local_dir: Option<PathBuf>,
+    docker_mount_dir: Option<String>,
+    docker_clobber: bool,
     remove_database: bool,
     script_type: Type,
 }
 
 lazy_static! {
     static ref CAPABILITIES: Regex =
-        Regex::new("^(?:.*[/\\\\])?ida(?P<mode>l|q|w)(?P<bits>(?:64)?)(?P<exe>(?:\\.exe)?)$").unwrap();
+        Regex::new("^(?:.*[/\\\\])?ida(?P<mode>l|q|w|t)(?P<bits>(?:64)?)(?P<exe>(?:\\.exe)?)$")
+            .unwrap();
+    static ref WINDOWS_PATH: Regex = Regex::new("^[A-Z]:").unwrap();
 }
 
 fn windowsify<S: AsRef<str>>(path: S) -> String {
     let path = path.as_ref();
-    if path.starts_with("/") { &path[1..] } else { path }.replace("/", "\\")
+    if WINDOWS_PATH.is_match(path.as_ref()) {
+        path.to_owned()
+    } else {
+        // NOTE: all paths are canonicalised, so we expect it to begin with /
+        let mut rpath = String::from(r"Z:\");
+        rpath.push_str(
+            if path.starts_with("/") {
+                &path[1..]
+            } else {
+                path
+            }
+            .replace("/", "\\")
+            .as_ref(),
+        );
+        rpath
+    }
 }
 
 /// IDA implements the core functionality of rida it provides a context with
@@ -97,21 +123,69 @@ impl IDA {
     /// 64-bit mode, whereas `idaq` will run with a graphical interface in
     /// 32-bit mode.
     pub fn new(ida_path: &str) -> Result<IDA, Error> {
-        CAPABILITIES.captures(ida_path)
+        CAPABILITIES
+            .captures(ida_path)
             .map(|caps| IDA {
                 exec: ida_path.to_owned(),
-                bits: if &caps["bits"] == "" { Bits::Bits32 } else { Bits::Bits64 },
-                mode: if &caps["mode"] == "l" { Mode::Headless } else { Mode::Graphical },
+                bits: if &caps["bits"] == "" {
+                    Bits::Bits32
+                } else {
+                    Bits::Bits64
+                },
+                mode: if &caps["mode"] == "l" || &caps["mode"] == "t" {
+                    Mode::Headless
+                } else {
+                    Mode::Graphical
+                },
+                docker_image: None,
+                docker_tag: None,
+                docker_local_dir: None,
+                docker_mount_dir: None,
+                docker_clobber: false,
                 wine: !caps["exe"].is_empty(),
                 remove_database: true,
                 script_type: Type::Python,
             })
-            .ok_or(IdaError::InvalidPath { path: ida_path.to_owned() }.into())
+            .ok_or(
+                IdaError::InvalidPath {
+                    path: ida_path.to_owned(),
+                }
+                .into(),
+            )
+    }
+
+    pub fn dockerised<R: AsRef<Path>>(
+        image: &str,
+        tag: &str,
+        local: R,
+        mount: &str,
+        ida_path: &str,
+    ) -> Result<IDA, Error> {
+        Self::new(ida_path).and_then(|i| i.with_docker(image, tag, local, mount))
     }
 
     /// Sets if the IDA database is removed upon script completion.
     pub fn remove_database(mut self, remove: bool) -> IDA {
         self.remove_database = remove;
+        self
+    }
+
+    pub fn with_docker<R: AsRef<Path>>(
+        mut self,
+        image: &str,
+        tag: &str,
+        local: R,
+        mount: &str,
+    ) -> Result<IDA, Error> {
+        self.docker_image = Some(image.to_owned());
+        self.docker_tag = Some(tag.to_owned());
+        self.docker_local_dir = Some(local.as_ref().canonicalize()?);
+        self.docker_mount_dir = Some(mount.to_owned());
+        Ok(self)
+    }
+
+    pub fn docker_clobbers(mut self, will_clobber: bool) -> IDA {
+        self.docker_clobber = will_clobber;
         self
     }
 
@@ -133,50 +207,151 @@ impl IDA {
         self.bits == Bits::Bits64
     }
 
+    /// Returns `true` if the IDA instance will be launched from a docker
+    /// container.
+    pub fn is_dockerised(&self) -> bool {
+        self.docker_image.is_some()
+    }
+
+    /// Returns `true` if IDA instance will be launched from wine or
+    /// wineconsole.
+    pub fn is_wine(&self) -> bool {
+        self.wine
+    }
+
     /// Runs the script with the contents given as `script` on the `target`
     /// executable.
     pub fn run<T: AsRef<Path>>(&self, script: &str, target: T) -> Result<bool, Error> {
-        let target = target.as_ref();
-        let mut script_file = tempfile::Builder::new()
-            .suffix(if self.script_type == Type::Python { ".py" } else { ".idc" })
-            .tempfile()?;
+        let target = target.as_ref().canonicalize()?;
+        let mut copied_target = false;
+        let mut orig_path = None;
+
+        let mut temp_builder = tempfile::Builder::new();
+        temp_builder.prefix("ida");
+        temp_builder.suffix(if self.script_type == Type::Python {
+            ".py"
+        } else {
+            ".idc"
+        });
+        let mut script_file = if let Some(ref dir) = self.docker_local_dir {
+            temp_builder.tempfile_in(dir)?
+        } else {
+            temp_builder.tempfile()?
+        };
 
         script_file.write(script.as_bytes())?;
         script_file.as_file().sync_all()?;
 
-        let mut cmd = if self.wine {
-            let mut cmd = process::Command::new("wine");
+        let (mut cmd, rscript, rtarget) = if self.is_dockerised() {
+            let mut cmd = process::Command::new("docker");
+            let local_dir = self.docker_local_dir.as_ref().unwrap();
+            let mount_dir = self.docker_mount_dir.as_ref().unwrap();
+            cmd.args(&[
+                "run",
+                "--rm",
+                "-t",
+                "-v",
+                &format!("{}:{}", local_dir.display(), mount_dir,),
+                &format!(
+                    "{}:{}",
+                    self.docker_image.as_ref().unwrap(),
+                    self.docker_tag.as_ref().unwrap(),
+                ),
+            ]);
+            if !self.is_wine() && self.is_headless() {
+                cmd.args(&["-e", "TVHEADLESS=1"]);
+            };
+            let rscript: String =
+                PathBuf::from_iter(&[mount_dir.as_ref(), script_file.path().file_name().unwrap()])
+                    .to_string_lossy()
+                    .into_owned();
 
-            let target_str = target.to_str().unwrap();
-            cmd.args(&[&self.exec, "-A", &format!("-S{}", windowsify(script_file.path().to_string_lossy())), &windowsify(target_str)]);
-            cmd
+            let rtarget = if let Ok(suffix) = target.strip_prefix(local_dir) {
+                let rtarget = PathBuf::from_iter(&[mount_dir.as_ref(), suffix]);
+                if self.remove_database {
+                    orig_path = Some(target)
+                };
+                rtarget
+            } else {
+                let file = target.file_name().ok_or_else(|| IdaError::InvalidTarget {
+                    path: target.to_owned(),
+                })?;
+                let to = PathBuf::from_iter(&[local_dir.as_ref(), file]);
+                // disallow clobbering
+                if !self.docker_clobber && to.exists() {
+                    return Err(IdaError::InvalidTargetClobber {
+                        path: target.to_owned(),
+                        clobber: to,
+                    }.into())
+                }
+                let rtarget = PathBuf::from_iter(&[mount_dir.as_ref(), file]);
+                fs::copy(target, &to)?;
+                copied_target = true;
+                orig_path = Some(to);
+                rtarget
+            };
+            (cmd, rscript, rtarget)
         } else {
-            let mut cmd = process::Command::new(&self.exec);
-
-            if self.mode == Mode::Headless {
+            let mut cmd = process::Command::new("/bin/sh");
+            cmd.arg("-c");
+            if !self.is_wine() && self.is_headless() {
                 cmd.env("TVHEADLESS", "1");
-            }
-
-            let target_str = target.to_str().unwrap();
-            cmd.args(&["-A", &format!("-S{}", script_file.path().display()), target_str]);
-            cmd
+            };
+            if self.remove_database {
+                orig_path = Some(target.to_owned())
+            };
+            (
+                cmd,
+                script_file.path().to_string_lossy().into_owned(),
+                target,
+            )
         };
+
+        if self.wine {
+            if self.is_headless() {
+                cmd.args(&["wineconsole", "--backend=curses"]);
+            } else {
+                cmd.arg("wine");
+            };
+
+            let target_str = rtarget.to_string_lossy();
+            cmd.args(&[
+                &self.exec,
+                "-A",
+                &format!("-S{}", windowsify(rscript)),
+                &windowsify(target_str),
+            ]);
+        } else {
+            cmd.arg(&self.exec);
+            let target_str = rtarget.to_string_lossy();
+            cmd.args(&["-A", &format!("-S{}", rscript), target_str.as_ref()]);
+        }
 
         let output = cmd.output()?;
 
+        if copied_target {
+            fs::remove_file(orig_path.as_ref().unwrap()).ok();
+        };
+
         if self.remove_database {
             // Can fail, in the case of, e.g., an unpacked database.
-            let target_path = format!("{}.{}", target.display(), if self.bits == Bits::Bits32 { "idb" } else { "i64" });
-            remove_file(&target_path).ok();
+            let target_path = format!(
+                "{}.{}",
+                orig_path.as_ref().unwrap().display(),
+                if self.bits == Bits::Bits32 {
+                    "idb"
+                } else {
+                    "i64"
+                }
+            );
+            fs::remove_file(&target_path).ok();
         }
 
         Ok(output.status.success())
     }
 
     pub fn function_names<T: AsRef<Path>>(&self, target: T) -> Result<Vec<String>, Error> {
-        let json = tempfile::Builder::new()
-            .suffix("json")
-            .tempfile()?;
+        let json = tempfile::Builder::new().suffix("json").tempfile()?;
         let path = json.into_temp_path();
         let command = format!(include_str!("../python/function_names.py"), path.display());
 
@@ -187,9 +362,7 @@ impl IDA {
     }
 
     pub fn function_boundaries<T: AsRef<Path>>(&self, target: T) -> Result<Vec<(u64, u64)>, Error> {
-        let json = tempfile::Builder::new()
-            .suffix("json")
-            .tempfile()?;
+        let json = tempfile::Builder::new().suffix("json").tempfile()?;
         let path = json.into_temp_path();
         let command = format!(include_str!("../python/function_starts.py"), path.display());
 
@@ -200,9 +373,7 @@ impl IDA {
     }
 
     pub fn function_cfgs<T: AsRef<Path>>(&self, target: T) -> Result<Vec<Function>, Error> {
-        let json = tempfile::Builder::new()
-            .suffix("json")
-            .tempfile()?;
+        let json = tempfile::Builder::new().suffix("json").tempfile()?;
         let path = json.into_temp_path();
         let command = format!(include_str!("../python/function_cfgs.py"), path.display());
 
